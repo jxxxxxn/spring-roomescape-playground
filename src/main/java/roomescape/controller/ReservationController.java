a120bd321f73071cf4a236186c6bf046d18ac0b0
jeein <23minjjj@ewha.ac.kr>
2025-10-04 21:44:15 +0900

    [Feat] reservation() Model 추가, 예약 초기 데이터 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,29 +2,32 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.dto.Reservation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class ReservationController {
 
-    private List<Reservation> reservations = new ArrayList<>();
+    Reservation reservation1=new Reservation(1L,"브라운","2023-01-01","10:00");
+    Reservation reservation2=new Reservation(2L,"브라운","2023-01-02","11:00");
+    private List<Reservation> reservations = new ArrayList<>(Arrays.asList(reservation1,reservation2));
 
     @GetMapping("/reservation")
-    public String reservation(){
+    public String reservation(Model model){
+
+        model.addAttribute(reservations);
         return "reservation";
     }
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> reservations(){
 
-        Reservation reservation1=new Reservation(1L,"브라운","2023-01-01","10:00");
-        reservations.add(reservation1);
-
         return ResponseEntity.ok(reservations);
-
     }
 }
