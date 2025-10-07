81259f3e5d80c368a7402bc0a7077bbfcd4c4fc6
jeein <23minjjj@ewha.ac.kr>
2025-10-07 18:57:10 +0900

    [Feat] dto 멤버변수 private으로 변경

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -6,10 +6,10 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Reservation {
-    public Long id;
-    public String name;
-    public String date; //우선 String으로.. 차후 Date로 형변환 필요하면 교체..
-    public String time; //동일!!
+    private Long id;
+    private String name;
+    private String date; //우선 String으로.. 차후 Date로 형변환 필요하면 교체..
+    private String time; //동일!!
 
     public Reservation(Long id,String name,String date,String time){
         this.id=id;
