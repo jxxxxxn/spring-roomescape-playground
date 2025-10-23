0ea5bfe3c41c1a2c66dac294200327a7301f4984
jeein <23minjjj@ewha.ac.kr>
2025-10-23 15:40:56 +0900

    [Fix] dto 멤버변수 private으로 변경했을 시 오류 수정 (@Getter 사용), @AllArgsConstructor 추가

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -1,20 +1,22 @@
 package roomescape.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Getter
+@AllArgsConstructor //기본 생성자 자동 생성
 public class Reservation {
+    //캡슐화를 위해서는 private으로 해야 하지만, step12의 코드에서 private으로 하면 JSON이 접근을 못 해서 직렬화 못 하는 에러 발생
+    //해결을 위해 lombok의 getter 추가
     private Long id;
     private String name;
     private String date; //우선 String으로.. 차후 Date로 형변환 필요하면 교체..
     private String time; //동일!!
 
-    public Reservation(Long id,String name,String date,String time){
-        this.id=id;
-        this.name=name;
-        this.date=date;
-        this.time=time;
-    }
+
 }
