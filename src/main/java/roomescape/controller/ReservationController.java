a4a9f6bcb8482269bfb2a232a217ec42a94855bd
jeein <23minjjj@ewha.ac.kr>
2025-10-23 17:20:42 +0900

    [Feat] 4단계 - 예외 처리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,14 +2,16 @@ package roomescape.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationAddReq;
 import roomescape.dto.ReservationReq;
+import roomescape.exception.InvalidRequestReservationException;
+import roomescape.exception.NotFoundReservationException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -19,7 +21,7 @@ public class ReservationController {
     //AtomicLong: Long 자료형 가지는 Wrapping 클래스. incrementAndGet(): ++x
     private AtomicLong index = new AtomicLong(0);
     private List<ReservationReq> reservations = new ArrayList<>();
-
+    
     @GetMapping("/reservation")
     public String reservation(Model model){
 
@@ -27,6 +29,7 @@ public class ReservationController {
         return "reservation";
     }
 
+    //예약 조회
     @GetMapping("/reservations")
     @ResponseBody
     public List<ReservationReq> reservations(){
@@ -34,10 +37,14 @@ public class ReservationController {
         return reservations;
     }
 
+    //예약 추가
     @PostMapping("/reservations")
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
     public ReservationReq addReservation(@RequestBody ReservationAddReq reservation, HttpServletResponse response){
+        if(reservation.getName().isEmpty()||reservation.getDate().isEmpty()||reservation.getTime().isEmpty()){
+            throw new InvalidRequestReservationException("필요한 인자가 없습니다.");
+        }
         ReservationReq newReservation =new ReservationReq(index.incrementAndGet(),reservation.getName(),reservation.getDate(),reservation.getTime());
         reservations.add(newReservation);
 
@@ -46,11 +53,17 @@ public class ReservationController {
         return newReservation;
     }
 
+    //예약 삭제
     @DeleteMapping("/reservations/{id}")
     @ResponseBody
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservation(@PathVariable Long id){
         ReservationReq delete=reservations.stream().filter(ReservationReq -> id.equals(ReservationReq.getId())).findAny().orElse(null);
+        if(delete==null){
+            throw new NotFoundReservationException("삭제할 예약이 없습니다");
+        }
         reservations.remove(delete);
     }
+
+
 }
