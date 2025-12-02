3bfe9cbee6be84b61900032a0804dff83177fef5
jeein <23minjjj@ewha.ac.kr>
2025-12-03 00:14:14 +0900

    [Feat] 10단계 - 계층화 리팩터링

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package roomescape.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,27 +12,20 @@ import roomescape.dto.ReservationAddReq;
 import roomescape.dto.ReservationReq;
 import roomescape.exception.InvalidRequestReservationException;
 import roomescape.exception.NotFoundReservationException;
+import roomescape.service.ReservationService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
 public class ReservationController {
+    @Autowired
+    private ReservationService reservationService;
 
-    //AtomicLong: Long 자료형 가지는 Wrapping 클래스. incrementAndGet(): ++x
-    private AtomicLong index = new AtomicLong(0);
-    private List<ReservationReq> reservations1 = new ArrayList<>();
-    private JdbcTemplate jdbcTemplate;
-
-    public ReservationController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    
     @GetMapping("/reservation")
     public String reservation(Model model){
 
-        model.addAttribute(reservations1);
+        //model.addAttribute(reservations1);
         return "new-reservation";
     }
 
@@ -39,19 +33,7 @@ public class ReservationController {
     @GetMapping("/reservations")
     @ResponseBody
     public List<ReservationReq> reservations(){
-        String sql="SELECT * from reservation";
-        List<ReservationReq> reservations=jdbcTemplate.query(
-                sql,
-                (resultSet,rowNum)->{
-                    ReservationReq reservation = new ReservationReq(
-                            resultSet.getLong("id"),
-                            resultSet.getString("name"),
-                            resultSet.getString("date"),
-                            resultSet.getTime("time")
-                    );
-                    return reservation;
-                });
-        return reservations;
+        return reservationService.getAllReservations();
     }
 
     //예약 추가
@@ -59,24 +41,8 @@ public class ReservationController {
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
     public ReservationReq addReservation(@RequestBody ReservationAddReq reservation, HttpServletResponse response){
-        if(reservation.getName().isEmpty()||reservation.getDate().isEmpty()){
-            throw new InvalidRequestReservationException("필요한 인자가 없습니다.");
-        }
-
-        String sql="SELECT \n" +
-                    "r.id as reservation_id, \n" +
-                    "r.name, \n" +
-                    "r.date, \n" +
-                    "t.id as time_id, \n" +
-                    "t.time as time_value \n" +
-                    "FROM reservation as r inner join time as t on r.time_id = t.id\n";
-        jdbcTemplate.update(sql,reservation.getName(),reservation.getDate(),reservation.getTime());
-        String getIdSql="SELECT LAST_INSERT_ID()"; //해당 구문 사용 위해 application.properties에 MODE=MySQL 추가함
-        Long id=jdbcTemplate.queryForObject(getIdSql, Long.class);
-        ReservationReq newReservation =new ReservationReq(id,reservation.getName(),reservation.getDate(),reservation.getTime());
-
-        // ResponseEntity 사용하면 header 명시적으로 지정하지 않아도 된다고 한다.
-        response.setHeader("Location", "/reservations/" + id);
+        ReservationReq newReservation = reservationService.addReservation(reservation);
+        response.setHeader("Location", "/reservations/" + newReservation.getId());
         return newReservation;
     }
 
@@ -85,13 +51,7 @@ public class ReservationController {
     @ResponseBody
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservation(@PathVariable Long id){
-
-        String sql="DELETE FROM reservation WHERE id=?";
-        int rowCount=jdbcTemplate.update(sql,id);
-        if(rowCount==0){
-            throw new NotFoundReservationException("삭제할 예약이 없습니다");
-        }
-
+        reservationService.deleteReservation(id);
     }
 
 }
